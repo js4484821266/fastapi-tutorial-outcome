e02f157d13a5ab0fb015fa0c02371c2c9d72d7c3
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2026-06-29 10:21:08 +0900

    complete path-params

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 import socket
 
 import uvicorn
+from enum import Enum
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -20,6 +21,23 @@ async def deohagi(a: float, b: float):
 @app.get("{a}/minus/{b}")
 async def ppaegi(a: float, b: float):
     return {"Ans": a - b}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
 
 
 def get_available_port(start=49152, end=65535):
